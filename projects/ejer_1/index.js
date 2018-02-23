a7a1576b619c4bda581d148822e7c43b96647add
francotesei <ftesei96@gmail.com>
2018-02-23 15:23:29 -0300

    added voting_app

--- projects/ejer_1/index.js
+++ projects/ejer_1/index.js
@@ -9,21 +9,21 @@ code = fs.readFileSync('Voting.sol').toString();
 compiledCode = solc.compile(code);
 abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface);
 VotingContract = web3.eth.contract(abiDefinition);
+
 byteCode = compiledCode.contracts[':Voting'].bytecode;
-deployedContract = VotingContract.new(['Rama','Nick','Jose'],
-{data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
-contractInstance = VotingContract.at(deployedContract.address);
-console.log("END")
+
+deployedContract = VotingContract.new(['Rama','Nick','Jose'],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
+console.log(deployedContract.address);
+//contractInstance = VotingContract.at(deployedContract.address);
 
 
-/*
-.load index.js
+//.load index.js
 
-contractInstance.totalVotesFor.call('Rama')
-contractInstance.voteForCandidate('Rama', {from: web3.eth.accounts[0]})
+//
 
-contractInstance.voteForCandidate('Rama', {from: web3.eth.accounts[0]})
-contractInstance.voteForCandidate('Rama', {from: web3.eth.accounts[0]})
-contractInstance.totalVotesFor.call('Rama').toLocaleString()
+//contractInstance.totalVotesFor.call('Rama')
+//contractInstance.voteForCandidate('Rama', {from: web3.eth.accounts[0]})
 
-*/
+//contractInstance.voteForCandidate('Rama', {from: web3.eth.accounts[0]})
+//contractInstance.voteForCandidate('Rama', {from: web3.eth.accounts[0]})
+//contractInstance.totalVotesFor.call('Rama').toLocaleString()
